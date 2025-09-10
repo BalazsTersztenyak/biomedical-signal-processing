bd46ac9c20396591474db81b9ce2f5c3297a4d78
galligergo <galli.gergo@gmail.com>
2025-09-10 11:29:21 +0200

    gitignore update (Data)

---
+++ Task_2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import os
+
+# Folder where your .csv files are
+folder = "Data/wearable-device-dataset-from-induced-stress-and-structured-exercise-sessions-1.0.1/Wearable_Dataset/AEROBIC/f01/"  # <-- change this
+
+signals = ["ACC", "BVP", "EDA", "HR", "IBI", "tags", "TEMP"]
+
+data = {}
+
+for sig in signals:
+    filename = os.path.join(folder, f"{sig}.csv")
+    data[f"f01_{sig}"] = np.loadtxt(filename, delimiter=",", dtype=str)
+
+# Example usage:
+f01_ACC = data["f01_ACC"]
+print(f01_ACC)
