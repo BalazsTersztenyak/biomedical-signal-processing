beb054f6c32079c36c7e4c6afdc7e73c85532e42
Balazs <tbalu912@gmail.com>
2025-09-10 14:58:25 +0200

    convert data to float

--- Task_2.py
+++ Task_2.py
@@ -11,5 +11,10 @@ for sig in signals:
     filename = os.path.join(folder, f"{sig}.csv")
     data[f"f01_{sig}"] = np.loadtxt(filename, delimiter=",", dtype=str)
 
-f01_ACC = data["f01_ACC"]
-print(f01_ACC)
+#f01_ACC = data["f01_ACC"]
+#print(f01_ACC)
+
+for key in data:
+    if key != "f01_tags":
+        data[key][1:] = data[key][1:].astype(float)
+        #print(f"{key}: {data[key].shape}", data[key][:5])
